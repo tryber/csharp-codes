6f7df914ab789d2c42e524e3b9dae84067b6fcad
Isaac Rahel <isaac_rahel@live.com>
2023-09-06 09:39:34 -0300

    Chore: upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿//Entrada de Dados
+int Age = int.Parse(Console.ReadLine());
+decimal Average = decimal.Parse(Console.ReadLine());
+bool Permission = bool.Parse(Console.ReadLine());
+string Name = Console.ReadLine();
+
+Console.WriteLine(Age);
+Console.WriteLine(Average);
+Console.WriteLine(Permission);
+Console.WriteLine(Name);
