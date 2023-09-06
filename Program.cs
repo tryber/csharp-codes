4d41d41cd650bbde1d54b7ee3088d9990105ec45
Isaac Rahel <isaac_rahel@live.com>
2023-09-06 10:02:13 -0300

    Chore: upload

--- Program.cs
+++ Program.cs
@@ -1,8 +1,29 @@
-﻿//ordem de execução
-int a = 5, b = 10, c = 15;
-int result1 = (a + b * c);
-Console.WriteLine("(" + a + " + " + b + " * " + c + ") = " + result1);
+﻿int a = 1;
+//incrementa + 1 ao valor de a
+a++; //substitui a instrução a = a + 1 
+Console.WriteLine("A = " + a);
 
-//utilizando parênteses
-int result2 = ((a + b) * c);
-Console.WriteLine("((" + a + " + " + b + ") * " + c + ") = " + result2);
+int b = 10;
+//decrementa o -1 ao valor b
+b--; //substitui a instrução b = b - 1
+Console.WriteLine("B = " + b);
+
+//incrementa qualquer valor à direita na variável à esquerda
+int c = 23;
+c += 15; //substitui a instrução c = c + 15 
+Console.WriteLine("C = " + c);
+
+//incrementa multiplicando qualquer valor à direita na variável à esquerda
+int e = 11;
+e *= 3; //substitui a instrução e = e * 3 
+Console.WriteLine("E = " + e);
+
+//decrementa dividindo qualquer valor à direita na variável à esquerda
+decimal f = 11;
+f /= 3; //substitui a instrução f = f / 3 
+Console.WriteLine("F = " + f.ToString("N2"));
+
+//decrementa com a operação de módulo de qualquer valor à direita na variável à esquerda
+decimal g = 11;
+g %= 3; //substitui a instrução g = g % 3 
+Console.WriteLine("G = " + g.ToString("N2"));
