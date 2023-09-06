6ea75aa7f606c6b6202bc4c1bfba8ddade6fd5cc
Isaac Rahel <isaac_rahel@live.com>
2023-09-06 10:00:49 -0300

    Chore: upload

--- Program.cs
+++ Program.cs
@@ -1,13 +1,24 @@
-﻿//esta variável será compilada como string
-var school = "Trybe";
+﻿//Adição
+int a = 50, b = 50;
+int result1 = a + b;
+Console.WriteLine(a + " + " + b + " = " + result1);
 
-//esta variável será compilada como int
-var semester = 1;
+//Subtração
+int c = 77, d = 21;
+int result2 = c - d;
+Console.WriteLine(c + " - " + d + " = " + result2);
 
-//esta variável será compilada como um tipo anônimo
-var student = new { name = "Jorge", age = 30 };
+//Multiplicação
+int e = 5, f = 5;
+int result3 = e * f;
+Console.WriteLine(e + " * " + f + " = " + result3);
 
-Console.WriteLine("Olá! Me chamo " + student.name + " " +
-                  "sou um estudante da escola " + school + " " +
-                  "estou no " + semester + "º semestre " +
-                  "e tenho " + student.age + " anos");
+//divisão
+int g = 90, h = 9;
+int result4 = g / h;
+Console.WriteLine(g + " / " + h + " = " + result4);
+
+//módulo
+int i = 36, j = 7;
+int result5 = i % j;
+Console.WriteLine("O resto da divisão de " + i + " por " + j + " é " + result5);
