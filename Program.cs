647633402850a52550aaa9eaa77a64022b66f6d4
Isaac Rahel <isaac_rahel@live.com>
2023-09-06 10:01:42 -0300

    Chore: upload

--- Program.cs
+++ Program.cs
@@ -1,24 +1,8 @@
-﻿//Adição
-int a = 50, b = 50;
-int result1 = a + b;
-Console.WriteLine(a + " + " + b + " = " + result1);
+﻿//ordem de execução
+int a = 5, b = 10, c = 15;
+int result1 = (a + b * c);
+Console.WriteLine("(" + a + " + " + b + " * " + c + ") = " + result1);
 
-//Subtração
-int c = 77, d = 21;
-int result2 = c - d;
-Console.WriteLine(c + " - " + d + " = " + result2);
-
-//Multiplicação
-int e = 5, f = 5;
-int result3 = e * f;
-Console.WriteLine(e + " * " + f + " = " + result3);
-
-//divisão
-int g = 90, h = 9;
-int result4 = g / h;
-Console.WriteLine(g + " / " + h + " = " + result4);
-
-//módulo
-int i = 36, j = 7;
-int result5 = i % j;
-Console.WriteLine("O resto da divisão de " + i + " por " + j + " é " + result5);
+//utilizando parênteses
+int result2 = ((a + b) * c);
+Console.WriteLine("((" + a + " + " + b + ") * " + c + ") = " + result2);
