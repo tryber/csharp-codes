f925d4c3303939de451c1223b91dfe5c6a4d17b6
Isaac Rahel <isaac_rahel@live.com>
2023-09-06 09:41:52 -0300

    Chore: upload

--- Program.cs
+++ Program.cs
@@ -1,5 +1,21 @@
-﻿//Exemplo de concatenação de Dados
-int Number = 10;
-string Name = "Trybe";
-Console.WriteLine("O valor da variável numero é: " + Number);
-Console.WriteLine("Eu sou um estudante " + Name + "!");
+﻿//ENTRADA DE DADOS
+Console.WriteLine("Boas vindas ao programa 'Calcula Lâmpada'!");
+Console.WriteLine("Informe o nome do cômodo: ");
+string convenient = Console.ReadLine();
+
+Console.WriteLine("Informe em metros a largura deste cômodo: ");
+decimal width = decimal.Parse(Console.ReadLine());
+
+Console.WriteLine("Informe em metros o comprimento deste cômodo: ");
+decimal length = decimal.Parse(Console.ReadLine());
+
+Console.WriteLine("Informe a potência em watts da lâmpada que será utilizada: ");
+int power = int.Parse(Console.ReadLine());
+
+//PROCESSAMENTO
+decimal squareMeter = width * length;
+decimal quotientX = power / 18M;
+decimal totalLightBulbs = squareMeter / quotientX;
+
+//SAIDA DE DADOS
+Console.WriteLine("Para iluminar o cômodo: " + convenient + " com " + squareMeter.ToString("N2") + " metros quadrados " + "será necessário a instalação de " + totalLightBulbs.ToString("N2") + " lâmpada(s)");
