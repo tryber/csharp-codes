2141b699d37f53dc5913bcfb26f82729aa6259cb
Danilo-Oliveira-Silva <danilo.silva@betrybe.com>
2023-09-15 14:32:21 -0300

    chore: example

---
+++ TestApi/Controllers/ClientController.cs
@@ -0,0 +1,21 @@
+
+namespace TestApi.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using TestApi.Core;
+
+[ApiController]
+[Route("clients")]
+public class ClientController : ControllerBase
+{
+    private static List<Client> _clients = new();
+    private static int _nextId = 1;
+
+    [HttpPost]
+    public ActionResult Create(ClientRequest request)
+    {
+        var client = request.CreateClient(_nextId++);
+        _clients.Add(client);
+
+        return StatusCode(201, client);
+    }
+}
