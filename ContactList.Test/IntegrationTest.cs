339c9e793f343420f24f0099e3b1c36d24f8c5d4
Danilo-Oliveira-Silva <danilo.silva@betrybe.com>
2023-09-28 11:29:17 -0300

    chore: s3 d2 l5 ex4

--- ContactList.Test/IntegrationTest.cs
+++ ContactList.Test/IntegrationTest.cs
@@ -10,19 +10,21 @@ using ContactList.Models;
 using ContactList.Services;
 using ContactList.Controllers;
 
-public class IntegrationTest: IClassFixture<WebApplicationFactory<Program>>
+public class IntegrationTest : IClassFixture<WebApplicationFactory<Program>>
 {
     public HttpClient _clientTest;
     public Mock<IContactService> mockService;
 
     public IntegrationTest(WebApplicationFactory<Program> factory)
     {
-         mockService = new Mock<IContactService>();
+        mockService = new Mock<IContactService>();
 
-        _clientTest = factory.WithWebHostBuilder(builder => {
-            builder.ConfigureServices(services => {
+        _clientTest = factory.WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureServices(services =>
+            {
                 var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(IContactService));
-                if (descriptor != null) 
+                if (descriptor != null)
                 {
                     services.Remove(descriptor);
                 }
@@ -38,8 +40,8 @@ public class IntegrationTest: IClassFixture<WebApplicationFactory<Program>>
         // Arrange 
 
         Person[] personMoq = new Person[2];
-        personMoq[0] = new Person { PersonId = 1, PersonName = "Maria", PersonEmail = "maria@betrybe.com", PersonPhone = "5511999999999"};
-        personMoq[1] = new Person { PersonId = 2, PersonName = "João", PersonEmail = "joao@betrybe.com", PersonPhone = "5511988888888"};
+        personMoq[0] = new Person { PersonId = 1, PersonName = "Maria", PersonEmail = "maria@betrybe.com", PersonPhone = "5511999999999" };
+        personMoq[1] = new Person { PersonId = 2, PersonName = "João", PersonEmail = "joao@betrybe.com", PersonPhone = "5511988888888" };
         mockService.Setup(s => s.getPersonList()).Returns(personMoq);
 
         // Act
@@ -56,5 +58,35 @@ public class IntegrationTest: IClassFixture<WebApplicationFactory<Program>>
         Assert.Equal(personMoq[0].PersonName, jsonResponse[0].PersonName);
         Assert.Equal(personMoq[0].PersonPhone, jsonResponse[0].PersonPhone);
         Assert.Equal(personMoq[0].PersonEmail, jsonResponse[0].PersonEmail);
+    }
+
+    [Theory(DisplayName = "Testando a rota /POST Person")]
+    [InlineData("/person")]
+    public async Task TestCreatePerson(string url)
+    {
+        // Arrange
+
+        Person personMoq = new Person { PersonId = 3, PersonName = "Rebeca", PersonEmail = "rebeca@betrybe.com", PersonPhone = "5511977777777" };
+        mockService.Setup(s => s.addPerson(It.IsAny<Person>())).Returns(personMoq);
+
+        // Act
+
+        var inputObj = new
+        {
+            PersonName = "Rebeca",
+            PersonEmail = "rebeca@betrybe.com",
+            PersonPhone = "5511977777777"
+        };
+        var response = await _clientTest.PostAsync(url, new StringContent(JsonConvert.SerializeObject(inputObj), System.Text.Encoding.UTF8, "application/json"));
+        var responseString = await response.Content.ReadAsStringAsync();
+        Person jsonResponse = JsonConvert.DeserializeObject<Person>(responseString)!;
+
+        // Assert
+
+        Assert.Equal(System.Net.HttpStatusCode.Created, response?.StatusCode);
+        Assert.Equal(personMoq.PersonId, jsonResponse.PersonId);
+        Assert.Equal(personMoq.PersonName, jsonResponse.PersonName);
+        Assert.Equal(personMoq.PersonPhone, jsonResponse.PersonPhone);
+        Assert.Equal(personMoq.PersonEmail, jsonResponse.PersonEmail);
     }
 }
