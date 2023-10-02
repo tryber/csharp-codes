b4301f46e4f6e76954927930578394e22fc7b374
Isaac Rahel <isaac_rahel@live.com>
2023-10-02 15:37:49 -0300

    chore: upload

--- Program.cs
+++ Program.cs
@@ -2,48 +2,12 @@
 {
     public static void Main()
     {
-        Console.WriteLine("Informe um número inteiro");
-        string? response = Console.ReadLine();
-        int number = 0;
-        var canConvert = Int32.TryParse(response, out number);
-
-        switch (number)
+        var count = 0;
+        while (count < 10)
         {
-            case > 0:
-                Console.WriteLine("maior que 0");
-                break;
-            case 0:
-                Console.WriteLine("igual a zero");
-                break;
-            default:
-                Console.WriteLine("menor que zero");
-                break;
+            Console.WriteLine("count " + count);
+            count++;
         }
-
-        Console.WriteLine(IdentifyPolygon(3));
     }
 
-    public static string IdentifyPolygon(int sideCount)
-    {
-        var name = string.Empty;
-        switch (sideCount)
-        {
-            case < 3:
-                name = "Não é um polígono";
-                break;
-            case 3:
-                name = "Triângulo";
-                break;
-            case 4:
-                name = "Quadrado";
-                break;
-            case 5:
-                name = "Pentágono";
-                break;
-            default:
-                name = "Polígono não identificado";
-                break;
-        }
-        return name;
-    }
 }
