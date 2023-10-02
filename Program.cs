707759cb66d7cf9aa965b48e6e33c69dc0b71d94
Isaac Rahel <isaac_rahel@live.com>
2023-10-02 16:07:50 -0300

    chore: upload

--- Program.cs
+++ Program.cs
@@ -2,14 +2,10 @@
 {
     public static void muldiDimArrays()
     {
-        // Somente instanciamos o array mais externo neste primeiro passo
-        // Repare que apenas o primeiro colchetes contém números
-        int[][] jaggedArray = new int[4][];
+        int[] expensesCost;
+        Console.WriteLine("Entre com o número de despesas: ");
+        int numberOfExpenses = getNumberOfExpenses();
 
-        // Agora precisamos instanciar um novo array para cada posição do array mais externo
-        jaggedArray[0] = new int[4] { 6, 6, 6, 6 };
-        jaggedArray[1] = new int[3] { 6, 6, 6 };
-        jaggedArray[2] = new int[5] { 6, 6, 6, 6, 6 };
-        jaggedArray[3] = new int[2] { 6, 6 };
+        expensesCost = new int[numberOfExpenses];
     }
 }
