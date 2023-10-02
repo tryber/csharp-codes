5b6593a07f875e6b5a76843ef6fde91e1413b632
Isaac Rahel <isaac_rahel@live.com>
2023-10-02 16:17:01 -0300

    chore: upload

--- Program.cs
+++ Program.cs
@@ -3,14 +3,26 @@
     static void Main(string[] args)
     {
         int[] expensesCost;
-        Console.WriteLine("Entre com o número de despesas: ");
-        int numberOfExpenses = getNumberOfExpenses();
 
+        int numberOfExpenses = getNumberOfExpenses();
         expensesCost = new int[numberOfExpenses];
+        for (int i = 0; i < numberOfExpenses; i++)
+        {
+            expensesCost[i] = getExpenseCostFromUser();
+        }
     }
 
     public static int getNumberOfExpenses()
     {
+        Console.WriteLine("Entre com o número de despesas: ");
+        string entry = Console.ReadLine();
+        int convertInt = Convert.ToInt32(entry);
+        return convertInt;
+    }
+
+    public static int getExpenseCostFromUser()
+    {
+        Console.WriteLine("Entre com a despesa: ");
         string entry = Console.ReadLine();
         int convertInt = Convert.ToInt32(entry);
         return convertInt;
