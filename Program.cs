c3f0dc22d8132375b476a3c6a0c604f1bc7d2099
Isaac Rahel <isaac_rahel@live.com>
2023-10-02 16:14:13 -0300

    fix

--- Program.cs
+++ Program.cs
@@ -1,11 +1,18 @@
-﻿class PlayingWithArrays
+﻿class Program
 {
-    public static void muldiDimArrays()
+    static void Main(string[] args)
     {
         int[] expensesCost;
         Console.WriteLine("Entre com o número de despesas: ");
         int numberOfExpenses = getNumberOfExpenses();
 
         expensesCost = new int[numberOfExpenses];
+    }
+
+    public static int getNumberOfExpenses()
+    {
+        string entry = Console.ReadLine();
+        int convertInt = Convert.ToInt32(entry);
+        return convertInt;
     }
 }
