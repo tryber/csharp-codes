a0453ebbeb6e57eaeeec1c073a7a82c0209a77da
Isaac Rahel <isaac_rahel@live.com>
2023-10-11 15:12:06 -0300

    chore: upload

--- Engine.cs
+++ Engine.cs
@@ -1,3 +1,5 @@
-﻿public class Engine : IStartable
+﻿public class Engine : IStartable, IStoppable
 {
+    public void Start() => Console.WriteLine("Engine started!");
+    public void Stop() => Console.WriteLine("Engine stopped!");
 }
