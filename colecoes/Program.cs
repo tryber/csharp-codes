1aad362857a84a308d113d112ee3f469c7f057ee
Danilo-Oliveira-Silva <danilo.silva@betrybe.com>
2023-10-11 16:39:07 -0300

    chore: S4

--- colecoes/Program.cs
+++ colecoes/Program.cs
@@ -5,11 +5,9 @@ public class Program
     public static void Main(string[] args)
     {
         string[] greetings = { "Hello World", "Hello LINQ", "Hello Trybe" };
-        
-        var items = from greeting in greetings
-                    select greeting;
-  
-        foreach (var item in items)
-            Console.WriteLine(item);
-        }
+
+        var numberOfItems = greetings.Count();
+    
+        Console.WriteLine(numberOfItems);
+    }
 }
