9d80cd8dd801546cde5f109a7467971e5b00fcb9
Danilo-Oliveira-Silva <danilo.silva@betrybe.com>
2023-10-11 16:04:35 -0300

    chore: EX1

---
+++ colecoes/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace colecoes;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<string> vehicles = new List<string>(){ "carro", "moto", "avião" };
+
+        foreach (var item in vehicles)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
