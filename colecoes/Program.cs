7328d3ff4ed9b84e6ccacfeea3f8c7b613503dce
Danilo-Oliveira-Silva <danilo.silva@betrybe.com>
2023-10-11 16:19:17 -0300

    chore: S4

--- colecoes/Program.cs
+++ colecoes/Program.cs
@@ -4,7 +4,7 @@ public class Program
     public static void Main(string[] args)
     {
         List<string> cars = new List<string>(){ "Fusca", "Brasília" };
-        cars.Remove("Fusca");
+        cars.RemoveAt(1);
 
         cars.ForEach(car => {
             Console.WriteLine(car);
