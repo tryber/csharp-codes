bc50d62d7cab11fc1b8158d103df326322b5a4a5
Isaac Rahel <isaac_rahel@live.com>
2023-10-11 14:50:38 -0300

    chore: upload

--- Rocket.cs
+++ Rocket.cs
@@ -1,13 +1,12 @@
 ﻿class Rocket
 {
     string Name { get; set; }
-    int Fuel { get; set }
+    int Fuel { get; set; } = 0;
     decimal Price { get; set; }
 
     public Rocket(string name, decimal price)
     {
         Name = name;
-        Fuel = 0;
         Price = price;
     }
 }
