9d7bdb31556bec10d82ecbe92fdaae34ecc201c0
Danilo-Oliveira-Silva <danilo.silva@betrybe.com>
2023-10-11 16:33:15 -0300

    chore: S4

--- colecoes/Program.cs
+++ colecoes/Program.cs
@@ -1,16 +1,32 @@
 ﻿namespace colecoes;
+
+internal class Artist
+{
+    public string? name { get; set; }
+    public int listeners { get; set; }
+}
+
 public class Program
 {
     public static void Main(string[] args)
     {
-        string[] games = {"Fortnite", "Valorant", "Destiny", "Call of Duty", "World of Warcraft"};
-
-        IEnumerable<string> filteredGames = from game in games
-                                          where game.Contains('a')
-                                          select game;
-        foreach(var game in filteredGames)
+        var artists = new List<Artist>
         {
-            Console.WriteLine(game);
+            new Artist { name = "Raul Seixas", listeners = 50000 },
+            new Artist { name = "Mozart", listeners = 15000 },
+            new Artist { name = "Elvis Presley", listeners = 25000 },
+            new Artist { name = "Bob Dylan", listeners = 30000 },
+            new Artist { name = "Guns N' Roses", listeners = 40000 },
+        };
+
+        var topListeners =
+               from artist in artists
+               where artist.listeners > 30000
+               select artist.name;
+
+        foreach(var artist in topListeners)
+        {
+            Console.WriteLine(artist);
         }
     }
 }
