9fda1be1cb4a49d90a0f2244fdcb921e991cabbb
Danilo-Oliveira-Silva <danilo.silva@betrybe.com>
2023-10-12 10:51:05 -0300

    chore: S4

--- colecoes/Program.cs
+++ colecoes/Program.cs
@@ -1,9 +1,6 @@
 ﻿namespace colecoes;
 
-public class Developer {
-    public string? Name { get; set; }
-    public string? ProgrammingLanguage { get; set; }
-}
+
 
 
 public class Program
@@ -11,28 +8,14 @@ public class Program
     public static void Main(string[] args)
     {
        
-        List<Developer> developers = new List<Developer>
-        {
-            new Developer { Name = "Julia", ProgrammingLanguage = "C#" },
-            new Developer { Name = "Paulo", ProgrammingLanguage = "Java" },
-            new Developer { Name = "Rebeca", ProgrammingLanguage = "C#" },
-            new Developer { Name = "Rodrigo", ProgrammingLanguage = "C#" },
-            new Developer { Name = "Joana", ProgrammingLanguage = "Java" },
-            new Developer { Name = "Alan", ProgrammingLanguage = "Python" }
-        };
+        List<string> stacksProjectA = new List<string> { "C#", "SQL Server", "JSON", "Microsserviços" };
+        List<string> stacksProjectB = new List<string> { "Java", "MySQL", "JSON", "WebAPI" };
+        List<string> stacksProjectC = new List<string> { "C#", "MySQL", "XML" };
 
-        var developerSkills = from developer in developers
-                              group developer by developer.ProgrammingLanguage into languages
-                              select languages;
+        var stacksProjectsABC = stacksProjectA.Union(stacksProjectB).Union(stacksProjectC);
 
-
-        foreach(var languageGroup in developerSkills)
-        {
-            Console.WriteLine("Linguagem: " + languageGroup.Key);
-            foreach(var developer in languageGroup)
-            {
-                Console.WriteLine("Pessoas desenvolvedoras: " + developer.Name);
-            }
+        foreach (var stack in stacksProjectsABC) {
+            Console.WriteLine(stack);        
         }
     }
 }
