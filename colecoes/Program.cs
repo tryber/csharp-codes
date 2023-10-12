33c7ce342aa9d096679a530ef72f1f2b72d870f4
Danilo-Oliveira-Silva <danilo.silva@betrybe.com>
2023-10-12 10:16:57 -0300

    chore: S4

--- colecoes/Program.cs
+++ colecoes/Program.cs
@@ -1,35 +1,46 @@
 ﻿namespace colecoes;
 
-public class Customer {
+public class Author {
     public int Id { get; set; }
     public string? Name { get; set; }
-    public string? City { get; set;}
-    public string? Password { get; set; }
 }
 
-public class CustomerBasicData {
+public class Book {
     public string? Name { get; set; }
-    public string? City { get; set;}
-    
+    public int AuthorId { get; set;}
+}
+
+public class BookDTO {
+    public string? BookName { get; set; }
+    public string? AuthorName { get; set; }
 }
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        List<Customer> customers = new List<Customer>
+        List<Author> authors = new List<Author>
         {
-            new Customer { Id = 1, Name = "Rebeca", City = "Recife", Password =  "706632"},
-            new Customer { Id = 2, Name = "José", City = "Manaus", Password =  "128620"},
-            new Customer { Id = 3, Name = "Sandra", City = "Salvador", Password =  "043407"}
+            new Author { Id = 1, Name = "Clarice Lispector"},
+            new Author { Id = 2, Name = "Jorge Amado" }
         };
 
-        var customersList = from customer in customers
-                            select new CustomerBasicData { Name = customer.Name, City = customer.City };
-
-        foreach(CustomerBasicData customer in customersList)
+        List <Book> books = new List<Book>
         {
-            Console.WriteLine(customer.Name + " - City: " +customer.City);
+            new Book { Name = "Capitães da Areia", AuthorId = 2},
+            new Book { Name = "Água Viva", AuthorId = 1},
+            new Book { Name = "A hora da Estrela", AuthorId = 1},
+            new Book { Name = "Cacau", AuthorId = 2},
+        };
+
+        var booksList = from book in books
+                        from author in authors
+                        where author.Id == book.AuthorId
+                        select new BookDTO { BookName = book.Name, AuthorName = author.Name };
+
+        foreach(BookDTO book in booksList)
+        {
+            Console.WriteLine(book.BookName + " - Author: " +book.AuthorName);
         }
     }
 }
