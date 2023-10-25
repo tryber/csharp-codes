3b335fc09f641b63dac6c8530de0e221684f25eb
Danilo-Oliveira-Silva <danilo.silva@betrybe.com>
2023-10-25 19:03:31 -0300

    chore: Seção 5 - Dia 1

--- ProjetoSql/Category.cs
+++ ProjetoSql/Category.cs
@@ -1,6 +1,8 @@
 namespace ProjetoSql;
 public class Category
 {
-    public int Id { get; set; }
-    public string? Name { get; set; }
+    public int CategoryId { get; set; }
+    public string Name { get;  set; }
+
+    public ICollection<Product> Products { get; set; }
 }
