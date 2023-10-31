4326b444db54d6ea29a9be4cd4d86e3289320df1
Isaac Rahel <isaac_rahel@live.com>
2023-10-31 13:39:56 -0300

    chore: upload

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -23,6 +23,7 @@ public class WeatherForecastController : ControllerBase
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
+        _logger.Log(LogLevel.Information, "Método GET acionado");
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
             Date = DateTime.Now.AddDays(index),
