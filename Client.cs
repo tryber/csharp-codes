7fce2a5313b2d0c494a507ca4e05577a20676a5c
Danilo-Oliveira-Silva <danilo.silva@betrybe.com>
2024-03-20 12:00:08 -0300

    fix: class

---
+++ Client.cs
@@ -0,0 +1,13 @@
+﻿class Client
+{
+  string Name { get; set; }
+  bool Active { get; set; }
+  decimal Balance { get; set; }
+
+  public Client(string name, decimal Balance)
+  {
+    Name = name;
+    Active = false;
+    Balance = Balance;
+  }
+}
