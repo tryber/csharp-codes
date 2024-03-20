a6b96db6a5d9ace33691c9077dc70c01ebd85c9d
Danilo-Oliveira-Silva <danilo.silva@betrybe.com>
2024-03-20 12:02:59 -0300

    fix: update

---
+++ Client.cs
@@ -0,0 +1,12 @@
+﻿class Client
+{
+  string Name { get; set; }
+  bool Active { get; set; } = true;
+  decimal Balance { get; set; }
+
+  public Client(string name, decimal Balance)
+  {
+    Name = name;
+    Balance = Balance;
+  }
+}
