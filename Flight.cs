9a030b799151d3e0e55db20381cc6213639b0bdd
Danilo-Oliveira-Silva <danilo.silva@betrybe.com>
2024-03-21 17:24:14 -0300

    fix: class

--- Flight.cs
+++ Flight.cs
@@ -4,7 +4,24 @@ public class Flight
 {
     public string FlightId { get; set; }
     public double Distance;
-    public IAirplane Airplane { get; set; }
+
+    private string _FlightType;
+    private IAirplane? _Airplane;
+    public IAirplane? Airplane { 
+        get { return _Airplane } 
+        set
+        {
+            if (value.GetType() == typeof(PassengerAirplane))
+            {
+                _FlIghtType = "Commercial";
+            }
+            else
+            {
+                _FlightType = "Cargo";
+            }
+            _Airplane = value;
+        } 
+    }
 
     public Flight(string FlightId, double Distance)
     {
